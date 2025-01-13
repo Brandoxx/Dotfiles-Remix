c58cfd62dfa4de2de1bedc5b760b405f5ff06784
Brandon <brandon.m.u@hotmail.com>
2025-01-13 13:57:18 -0600

    Added nvim to dotfiles

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,7 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 if uwsm check may-start && uwsm select; then
 	exec systemd-cat -t uwsm_start uwsm start default
 end
+
 alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/.git/" --work-tree="$HOME/.dotfiles"'
 set GBM_BACKEND nvidia-drm
 set __GLX_VENDOR_LIBRARY_NAME nvidia
